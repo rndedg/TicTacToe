2a626bd7447d74e4d144c5648bc18b82cab3dc8b
Gord L <rndedg@gmail.com>
2023-03-29 10:00:13 -0700

    Added result validation to check if the game is active or has been won, lost or a draw.

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 /*
 Used to store game status to make it easier to track later on.
 */
-const statusDisplay = document.querySelector('.game-status');
+const statusDisplay = document.querySelector(".game-status");
 /*
 Here we declare some variables that we will use to track the 
 game state throught the game. 
@@ -37,31 +37,67 @@ function handleCellPlayed(clickedCell, clickedCellIndex) {
   gameState[clickedCellIndex] = currentPlayer;
   clickedCell.innerHTML = currentPlayer;
 }
-function handlePlayerChange() {
+function handlePlayerChange() {}
 
-}
+const winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 function handleResultValidation() {
+  let roundWon = false;
 
+  for (let i = 0; i <= 7; i++) {
+    const winningCondition = winningConditions[i];
+    let a = gameState[winningCondition[0]];
+    let b = gameState[winningCondition[1]];
+    let c = gameState[winningCondition[2]];
+
+    if (a === "" || b === "" || c === "") {
+      continue;
+    }
+    if (a === b && b === c) {
+      roundWon = true;
+      break;
+    }
+  }
+
+  if (roundWon) {
+    statusDisplay.innerHTML = winningMessage();
+    gameActive = false;
+    return;
+  }
 }
+
 function handleCellClick(clickedCellEvent) {
   const clickedCell = clickedCellEvent.target;
-  const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
+  const clickedCellIndex = parseInt(
+    clickedCell.getAttribute("data-cell-index")
+  );
 
   if (gameState[clickedCellIndex] !== "" || !gameActive) {
     return;
   }
-  
-  console.log(`You have clicked cell number ${clickedCellIndex}`)
+
+  console.log(`You have clicked cell number ${clickedCellIndex}`);
   handleCellPlayed(clickedCell, clickedCellIndex);
   handleResultValidation();
 }
 
-function handleRestartGame() {
-
-}
+function handleRestartGame() {}
 /*
 And finally we add our event listeners to the actual game cells, as well as our 
 restart button
 */
-document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
-document.querySelector('.game-restart').addEventListener('click', handleRestartGame);
+document
+  .querySelectorAll(".cell")
+  .forEach((cell) => cell.addEventListener("click", handleCellClick));
+document
+  .querySelector(".game-restart")
+  .addEventListener("click", handleRestartGame);
